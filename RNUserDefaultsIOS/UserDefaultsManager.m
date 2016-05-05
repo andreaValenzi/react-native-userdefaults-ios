0eef7a2cc68620bcad1f09863475efa3564f57ca
andreaValenzi <andrew.valenzi@gmail.com>
2016-05-05 12:15:15 +0200

    shared container

--- RNUserDefaultsIOS/UserDefaultsManager.m
+++ RNUserDefaultsIOS/UserDefaultsManager.m
@@ -8,41 +8,55 @@
 
 #import "UserDefaultsManager.h"
 
+@interface UserDefaultsManager()
+
++ (NSUserDefaults *)container;
+
+@end
+
 @implementation UserDefaultsManager
+
++ (NSUserDefaults *)container {
+    static NSUserDefaults *fooContainer = nil;
+    if (fooContainer == nil) {
+        fooContainer = [[NSUserDefaults alloc] initWithSuiteName:@"group.com.rebrandly"];
+    }
+    return fooContainer;
+}
 
 + (void)setObject:(id)object forKey:(NSString *)key {
     
-    [[NSUserDefaults standardUserDefaults] setObject:object forKey:key];
+    [[UserDefaultsManager container] setObject:object forKey:key];
 }
 
 + (void)setBool:(bool)value forKey:(NSString *)key {
     
-    [[NSUserDefaults standardUserDefaults] setBool:value forKey:key];
+    [[UserDefaultsManager container] setBool:value forKey:key];
 }
 
 + (void)removeObjectForKey:(NSString *)key {
     
-    [[NSUserDefaults standardUserDefaults] removeObjectForKey:key];
+    [[UserDefaultsManager container] removeObjectForKey:key];
 }
 
 + (NSArray *)arrayForKey:(NSString *)key {
     
-    return [[NSUserDefaults standardUserDefaults] arrayForKey:key];
+    return [[UserDefaultsManager container] arrayForKey:key];
 }
 
 + (NSString *)stringForKey:(NSString *)key {
     
-    return [[NSUserDefaults standardUserDefaults] stringForKey:key];
+    return [[UserDefaultsManager container] stringForKey:key];
 }
 
 + (id)objectForKey:(NSString *)key {
     
-    return [[NSUserDefaults standardUserDefaults] objectForKey:key];
+    return [[UserDefaultsManager container] objectForKey:key];
 }
 
 + (bool)boolForKey:(NSString *)key {
     
-    return [[NSUserDefaults standardUserDefaults] boolForKey:key];
+    return [[UserDefaultsManager container] boolForKey:key];
 }
 
 @end
